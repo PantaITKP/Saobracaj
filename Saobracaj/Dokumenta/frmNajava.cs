25a1457b31978331702f88ca159daf60916bfadf
stefanObradovic <95360380+stefanObradovic@users.noreply.github.com>
2022-04-26 15:06:42 +0200

    Promene koje je Cpajak napisao u mail-u za Prekovremeni Rad. Dorada na promeni statusa 7 i 9 na najavi ukoliko se vrsi prazan transport. Deo PPK jos nije zavrseno

--- Saobracaj/Dokumenta/frmNajava.cs
+++ Saobracaj/Dokumenta/frmNajava.cs
@@ -834,8 +834,10 @@ namespace Saobracaj.Dokumenta
                 int sp = Convert.ToInt32(cboStatusPredaje.SelectedValue);
                 if (sp == 7 || sp == 9)
                 {
-                    if (cboPosiljalac.Text =="" || cboPrimalac.Text=="" || cboPrevoznik.Text=="" || cboPrevoznikZa.Text=="" || cboOtpravna.Text == "" || cboUputna.Text == "" || 
-                        cboGranicna.Text == "" || cboTipPrevoza.Text == "Nije definisan" || txtNetoTezina.Value == 0 || txtNetoTezinaM.Value ==0|| txtBrojKola.Value==0 || txtDuzinaM.Value==0)
+                    if (cboPosiljalac.Text =="" || cboPrimalac.Text=="" || cboPrevoznik.Text=="" || cboPrevoznikZa.Text=="" || cboOtpravna.Text == "" || 
+                        cboUputna.Text == "" || cboGranicna.Text == "" || cboTipPrevoza.Text == "Nije definisan" || 
+                        cboTipPrevoza.Text!= "Unutrašnji prazan" || cboTipPrevoza.Text!= "Uvoz/Izvoz/Tranzit prazno"
+                        || txtNetoTezina.Value == 0 || txtNetoTezinaM.Value ==0|| txtBrojKola.Value==0 || txtDuzinaM.Value==0)
                     {
                         MessageBox.Show("Moraju se popuniti sva obavezna polja:\n-Pošiljalac \n-Primalac \n-Otrpavna \n-Uputna \n-Trenutna \n-Preuzima od \n-Predaje za" +
                             "\n-Tip Prevoza \n-Bruto \n-Neto \n-Dužina \n-Broj Kola","Ispravnost unetih polja", MessageBoxButtons.OK, MessageBoxIcon.Warning);
---
+++ Saobracaj/Dokumenta/frmPPKGlavna.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Saobracaj.Dokumenta
+{
+    public partial class frmPPKGlavna : Form
+    {
+        public frmPPKGlavna()
+        {
+            InitializeComponent();
+            FillGV();
+        }
+        private void FillGV()
+        {
+            var select = "Select Aktivnosti.ID as [Aktivnost],(Rtrim(DePriimek) + ' ' + RTrim(DeIme)) as Zaposleni,VremeOD,VremeDo,Opis,AktivnostiStavke.ID as [Aktivnosti Stavke]," +
+                "Napomena,DatumZavrsetka " +
+                "From Aktivnosti " +
+                "Inner join AktivnostiStavke on Aktivnosti.ID = AktivnostiStavke.IDNadredjena " +
+                "Inner join Delavci on Aktivnosti.Zaposleni = Delavci.DeSifra " +
+                "Where VrstaAktivnostiID = 9 order by Aktivnosti.ID desc";
+
+            var s_connection = ConfigurationManager.ConnectionStrings["WindowsFormsApplication1.Properties.Settings.NedraConnectionString"].ConnectionString;
+            SqlConnection myConnection = new SqlConnection(s_connection);
+            var c = new SqlConnection(s_connection);
+            var dataAdapter = new SqlDataAdapter(select, c);
+
+            var commandBuilder = new SqlCommandBuilder(dataAdapter);
+            var ds = new DataSet();
+            dataAdapter.Fill(ds);
+            dataGridView1.ReadOnly = true;
+            dataGridView1.DataSource = ds.Tables[0];
+        }
+    }
+}
