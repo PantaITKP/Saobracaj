5bcb574cde82552a2743f3d685979a04f141339d
stefanObradovic <95360380+stefanObradovic@users.noreply.github.com>
2022-04-28 12:26:47 +0200

    PPK

--- Saobracaj/Dokumenta/frmPPKGlavna.cs
+++ Saobracaj/Dokumenta/frmPPKGlavna.cs
@@ -14,17 +14,20 @@ namespace Saobracaj.Dokumenta
 {
     public partial class frmPPKGlavna : Form
     {
+        int zaposleniID;
         public frmPPKGlavna()
         {
             InitializeComponent();
             FillGV();
+            txt_ID.Enabled = false;
+            txt_Zaposleni.Enabled = false;
         }
         private void FillGV()
         {
-            var select = "Select Aktivnosti.ID as [Aktivnost],(Rtrim(DePriimek) + ' ' + RTrim(DeIme)) as Zaposleni,VremeOD,VremeDo,Opis,AktivnostiStavke.ID as [Aktivnosti Stavke]," +
-                "Napomena,DatumZavrsetka " +
-                "From Aktivnosti " +
-                "Inner join AktivnostiStavke on Aktivnosti.ID = AktivnostiStavke.IDNadredjena " +
+            var select = "Select TOP 1000 IDNadredjena as ID,(Rtrim(DePriimek) + ' ' + RTrim(DeIme)) as Zaposleni,Aktivnosti.VremeOD,Aktivnosti.VremeDo,Aktivnosti.Opis,Napomena,Razlog," +
+                "DatumZavrsetka,Aktivnosti.Zaposleni " +
+                "From AktivnostiStavke " +
+                "Inner join Aktivnosti on AktivnostiStavke.IDNadredjena = Aktivnosti.ID " +
                 "Inner join Delavci on Aktivnosti.Zaposleni = Delavci.DeSifra " +
                 "Where VrstaAktivnostiID = 9 order by Aktivnosti.ID desc";
 
@@ -38,6 +41,51 @@ namespace Saobracaj.Dokumenta
             dataAdapter.Fill(ds);
             dataGridView1.ReadOnly = true;
             dataGridView1.DataSource = ds.Tables[0];
+
+            dataGridView1.Columns[0].Width = 60;
+            dataGridView1.Columns[1].Width = 120;
+            dataGridView1.Columns[4].Width = 180;
+            dataGridView1.Columns[6].Width = 120;
+            //dataGridView1.Columns[8].Visible = false;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_PPK_Click(object sender, EventArgs e)
+        {
+            
+            if (txt_ID.Text == "")
+            {
+                MessageBox.Show("Mora se izabrati zapis");
+            }
+            else
+            {
+                Dokumenta.frmPPK ppk = new frmPPK(Convert.ToInt32(txt_ID.Text), zaposleniID);
+                ppk.Show();
+            }
+        }
+
+        private void dataGridView1_SelectionChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                foreach (DataGridViewRow row in dataGridView1.Rows)
+                {
+                    if (row.Selected)
+                    {
+                        txt_Zaposleni.Text = row.Cells[1].Value.ToString().TrimEnd();
+                        txt_ID.Text = row.Cells[0].Value.ToString();
+                        zaposleniID = Convert.ToInt32(row.Cells[8].Value.ToString());
+                    }
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Nije uspela selekcija stavki");
+            }
         }
     }
 }
